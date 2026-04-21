5b804f3cc58d1f028d1836f9380d4b6cfa2eaf34
Vitaly Orekhov <vkvo2000@vivaldi.net>
2026-04-21 22:23:12 +0300

    Add language and time format settings

---
+++ Valour/Client/Utility/ResxLocalizer.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Collections;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Valour.Sdk.Services;
+
+namespace Valour.Client.Utility
+{
+    public class ResxLocalizer<T>: IStringLocalizer<T>
+    {
+        private readonly ResourceManager _resMan;
+
+        public ResxLocalizer()
+        {
+            Type typeOfT = typeof(T);
+
+            string fullName = typeOfT.FullName;
+            fullName = fullName.Substring("Valour.Client.".Count());
+
+            _resMan = new ResourceManager("Valour.Client.Resources." + fullName, Assembly.GetExecutingAssembly());
+        }
+
+        public LocalizedString this[string name]
+        {
+            get { return this[name, []]; }
+        }
+
+        public LocalizedString this[string name, params object[] args]
+        {
+            get
+            {
+                string format;
+                
+                try
+                {
+                    format = _resMan.GetString(name, CultureInfo.CurrentUICulture);
+                }
+                catch (MissingManifestResourceException)
+                {
+                    return new LocalizedString(name, name);
+                }
+
+                string value = string.Format(format ?? name, args);
+
+                return new LocalizedString(name, value);
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeAncestorCulture)
+        {
+            var resSet = _resMan.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            if (resSet != null)
+            {
+                foreach(DictionaryEntry entry in resSet)
+                {
+                    yield return new LocalizedString(entry.Key.ToString(), entry.Value?.ToString() ?? string.Empty);
+                }
+            }
+        }
+    }
+}
---
+++ Valour/Database/Migrations/20260503104213_MUISyncBetweenDevices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Valour.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class MUISyncBetweenDevices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "sync_language_between_devices",
+                table: "user_preferences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sync_language_between_devices",
+                table: "user_preferences");
+        }
+    }
+}
